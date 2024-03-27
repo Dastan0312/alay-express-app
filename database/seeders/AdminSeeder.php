aa79251fcf7bd2da6d4cfc7e8e3c4dedfe87c5ba
dastan.koich <sqrt111@gmail.com>
2024-03-27 13:29:55 +0600

    implement authentication and add initial values

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('admin123'),
+            'email_verified_at' => now(),
+            'role_id' => 1, 
+        ]);
+    }
+}
